cb978e39332408ca7094d8e5467b3a2423295b9a
nourhene <benothmennourhen8@gmail.com>
2025-08-04 20:40:46 +0100

    🔧 Mise à jour des fonctions register/login utilisateur avec vérification email, hash, et meilleure réponse API

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,6 +1,5 @@
 const userModel = require('../models/userModel');
 const bcrypt = require('bcrypt');
-const userModel = require('../models/userModel');
 
 exports.register = async (req, res) => {
   try {
