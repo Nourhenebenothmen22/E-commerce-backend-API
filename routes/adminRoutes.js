36737f6a7974a38f1189af2c30430f14650aa54c
nourhene <benothmennourhen8@gmail.com>
2025-08-06 20:48:33 +0100

    feat(adminRoutes): sécurisation des routes sensibles avec authMiddleware

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const router = express.Router();
-const{addAdmin, deleteAdmin, listAdmins, updateAdmin, getAdminById,countAdmins} = require('../controllers/adminControllers');
-const authMiddleware = require('../middleware/authMiddleware');
-router.post('/add', addAdmin);
-router.delete('/delete/:id', deleteAdmin);
-router.get('/list',authMiddleware, listAdmins);
-router.put('/update/:id', updateAdmin);
-router.get('/:id', getAdminById);
-router.get('/count',countAdmins)
+const {
+  addAdmin,
+  deleteAdmin,
+  listAdmins,
+  updateAdmin,
+  getAdminById,
+  countAdmins  
+} = require('../controllers/adminControllers'); // Chemin complet ajouté
+const authMiddleware = require('../middleware/authMiddleware'); 
+
+// Appliquez authMiddleware uniquement aux routes nécessaires
+router.post('/add', authMiddleware, addAdmin);
+router.get('/count', countAdmins); // Pas de middleware nécessaire
+router.get('/list', listAdmins);
+router.delete('/delete/:id', authMiddleware, deleteAdmin);
+router.put('/update/:id', authMiddleware, updateAdmin);
+router.get('/:id', authMiddleware, getAdminById);
+
 module.exports = router;
