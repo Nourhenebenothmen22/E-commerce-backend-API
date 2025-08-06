728593826ee35440bcba1db50f903a4ddf908a9e
nourhene <benothmennourhen8@gmail.com>
2025-08-06 19:56:26 +0100

    feat(adminRoutes): ajout d’un middleware d’authentification pour sécuriser l’accès à la liste des admins

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const{addAdmin, deleteAdmin, listAdmins, updateAdmin, getAdminById,countAdmins} = require('../controllers/adminControllers');
+const authMiddleware = require('../middleware/authMiddleware');
 router.post('/add', addAdmin);
 router.delete('/delete/:id', deleteAdmin);
-router.get('/list', listAdmins);
+router.get('/list',authMiddleware, listAdmins);
 router.put('/update/:id', updateAdmin);
 router.get('/:id', getAdminById);
 router.get('/count',countAdmins)
